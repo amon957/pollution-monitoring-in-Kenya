4923bc09f9205c047e2312af7e3c08a095aec267
Amon Melly <55048911+amon957@users.noreply.github.com>
2023-11-08 00:51:27 +0300

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -5,6 +5,6 @@ def authenticate(ee):
     key = json2.loads(secret_key)
     service_account=st.secrets.account
 
-    credentials = ee.ServiceAccountCredentials(service_account, key)
+    credentials = ee.ServiceAccountCredentials(service_account, secret_key)
     ee.Initialize(credentials)
     return(ee)
